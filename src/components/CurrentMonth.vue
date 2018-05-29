1274f8fc5844216990a3e2695d605e12e8a88d86
John Ale <john.richard.ale@gmail.com>
2018-05-29 11:26:14 -0400

    added buttons to change the calendar's year

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
-        <div>{{ formattedDate }}</div>
-        <button @click="dec">-</button>
-        <button @click="inc">+</button>
+        <button @click="decYear">&laquo;</button>
+        <button @click="decMonth">&lsaquo;</button>
+        <div class="date">{{ formattedDate }}</div>
+        <button @click="incMonth">&rsaquo;</button>
+        <button @click="incYear">&raquo;</button>
     </div>
 </template>
 <script>
     export default {
         methods: {
-            dec() {
+            jumpToPresent() {
+                let month = this.$moment().month() + 1;
+                let year = this.$moment().year();
+                this.$store.commit('setCurrentMonth', month);
+                this.$store.commit('setCurrentYear', year);
+            },
+            decMonth() {
                 if (this.month === 1) {
                     this.$store.commit('setCurrentMonth', 12);
                     this.$store.commit('setCurrentYear', this.year - 1);
@@ -17,13 +25,21 @@
                 }
                 this.$store.commit('eventFormActive', false);
             },
-            inc() {
+            decYear() {
+                this.$store.commit('setCurrentYear', this.year - 1);
+                this.$store.commit('eventFormActive', false);
+            },
+            incMonth() {
                 if (this.month === 12) {
                     this.$store.commit('setCurrentMonth', 1);
                     this.$store.commit('setCurrentYear', this.year + 1);
                 } else {
                     this.$store.commit('setCurrentMonth', this.month + 1);
                 }
+                this.$store.commit('eventFormActive', false);
+            },
+            incYear() {
+                this.$store.commit('setCurrentYear', this.year + 1);
                 this.$store.commit('eventFormActive', false);
             }
         },
